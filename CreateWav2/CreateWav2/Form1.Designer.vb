e857286f6c451718593acff0ea1438a548d98852
hxb <mybmob@sina.com>
2017-05-22 15:14:21 +0800

    first send file

---
+++ CreateWav2/CreateWav2/Form1.Designer.vb
@@ -0,0 +1,321 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(10, 186)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "生成Wav文件"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(14, 56)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(89, 12)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "采样频率（Hz）"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(14, 24)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(89, 12)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "信号时长（秒）"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(14, 115)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(95, 12)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "通道数（默认1）"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(122, 21)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.Text = "1"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(122, 115)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox2.TabIndex = 5
+        Me.TextBox2.Text = "1"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(122, 54)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox3.TabIndex = 6
+        Me.TextBox3.Text = "441000"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(14, 85)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 12)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "采样位数"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(122, 82)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox4.TabIndex = 8
+        Me.TextBox4.Text = "8"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(122, 186)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox5.TabIndex = 9
+        Me.TextBox5.Text = "Test1.wav"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(14, 152)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(77, 12)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "生成波形频率"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(122, 149)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox6.TabIndex = 11
+        Me.TextBox6.Text = "1000"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox1.Location = New System.Drawing.Point(243, 21)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(647, 82)
+        Me.PictureBox1.TabIndex = 12
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox2.Location = New System.Drawing.Point(243, 120)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(647, 87)
+        Me.PictureBox2.TabIndex = 13
+        Me.PictureBox2.TabStop = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(241, 6)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(65, 12)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "左声道波形"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(241, 105)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(65, 12)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "右声道波形"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(789, 218)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(101, 23)
+        Me.Button2.TabIndex = 16
+        Me.Button2.Text = "选择wav文件"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "Test.wav"
+        Me.OpenFileDialog1.InitialDirectory = "D:\"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(243, 218)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(121, 23)
+        Me.Button3.TabIndex = 17
+        Me.Button3.Text = "显示生成文件波形"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(474, 219)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(34, 21)
+        Me.TextBox7.TabIndex = 18
+        Me.TextBox7.Text = "1.0"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(385, 224)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(83, 12)
+        Me.Label8.TabIndex = 19
+        Me.Label8.Text = "设置x轴的比例"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(524, 223)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(83, 12)
+        Me.Label9.TabIndex = 21
+        Me.Label9.Text = "设置y轴的比例"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Enabled = False
+        Me.TextBox8.Location = New System.Drawing.Point(613, 218)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(34, 21)
+        Me.TextBox8.TabIndex = 20
+        Me.TextBox8.Text = "5"
+        '
+        'Button4
+        '
+        Me.Button4.Enabled = False
+        Me.Button4.Location = New System.Drawing.Point(694, 218)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 22
+        Me.Button4.Text = "刷新图形"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(957, 286)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = " WAV音频文件生成查看器"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Button4 As Button
+End Class
---
+++ CreateWav2/CreateWav2/Form1.vb
@@ -0,0 +1,217 @@
+﻿Imports System.IO
+Imports System.Math
+
+
+Public Class Form1
+    Structure waveHeaderStructre
+        'RiffChunk
+        Dim RIFF() As Char
+        Dim FileSize As UInteger
+        Dim WAVE() As Char
+        'FormatChunk
+        Dim FORMAT() As Char
+        Dim FormatSize As UInteger
+        Dim FilePadding As UShort
+        Dim FormatChannels As UShort
+        Dim SamplesPerSecond As UInteger
+        Dim AverageBytesPerSecond As UInteger
+        Dim BytesPerSample As UShort
+        Dim BitsPerSample As UShort
+        'Dim FormatExtra As UShort
+        'FactChunk
+        ' Dim FACT As String
+        ' Dim FactSize As UInteger
+        ' Dim FactInf As UInteger
+        'DataChunk
+        Dim DATA() As Char
+        Dim DataSize As UInteger
+    End Structure
+    Dim bshowFilename As String
+    ' Private sub change2normal
+    Private Sub showWave(picbox As PictureBox, ByVal data() As Byte)
+        Dim g As Graphics = picbox.CreateGraphics
+        g.Clear(SystemColors.ControlLight)
+        Dim axialPen As Pen = New Pen(Color.Blue, 1)
+        Dim picXdot As Integer
+        Dim picYdot As Integer
+        picYdot = Int(picbox.Height / 2)
+        picXdot = 5
+        '变换为数学坐标系
+        g.TranslateTransform(picXdot, picYdot)
+        g.ScaleTransform(1, -1)
+        Dim startX As Integer = -5
+        Dim endX As Integer = picbox.Width - 10
+        Dim startY As Integer = 0
+        Dim endY As Integer = 0
+        'draw x axial
+        g.DrawLine(axialPen, startX, startY, endX, endY)
+        'draw 箭头
+        g.DrawLine(axialPen, endX - 5, 5, endX, endY)
+        g.DrawLine(axialPen, endX - 5, -5, endX, endY)
+        'drawing Y axial
+        g.DrawLine(axialPen, 0, -picYdot, 0, picYdot)
+        g.DrawLine(axialPen, -5, picYdot - 5, 0, picYdot)
+        g.DrawLine(axialPen, 5, picYdot - 5, 0, picYdot)
+        Dim mBrush As New SolidBrush(Color.Blue)
+        Dim mFont As New Font("宋体", 10)
+        g.DrawString("x", mFont, mBrush, endX - 10, 5)
+        For x = 0 To endX - 10 Step 10
+            g.DrawLine(axialPen, x, 0, x, 2)
+        Next
+        For y = -picYdot + 10 To picYdot - 10 Step 10
+            g.DrawLine(axialPen, 0, y, 2, y)
+        Next
+        'up code drawing axial finshed
+        ' drawing wave start
+        Dim bpen As Pen = New Pen(Color.Red, 1)
+        Dim y1 As Integer
+        Dim y2 As Integer
+        Dim x1 As Integer
+        Dim x2 As Integer
+        Dim xdiv As Double
+        xdiv = TextBox7.Text
+        Dim ydiv As Double
+        ydiv = TextBox8.Text
+        For x = 0 To data.Length - 2
+            x1 = Int(x / xdiv)
+            y1 = Int(data(x) / ydiv) - picYdot / 2
+            x2 = Int((x + 1) / xdiv)
+            y2 = Int(data(x + 1) / ydiv) - picYdot / 2
+            g.DrawLine(bpen, x1, y1, x2, y2)
+        Next
+        g.Dispose()
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim sampleTime, sampleHz, channel, channelBit, happenHz As Integer
+        sampleTime = TextBox1.Text
+        sampleHz = TextBox3.Text
+        channel = TextBox2.Text
+        channelBit = TextBox4.Text
+        happenHz = TextBox6.Text
+        If (Int(sampleHz / happenHz) < 2) Then
+            MsgBox("采样频率必须是生成频率的2倍以上！")
+            Return
+        End If
+        Dim fileSizeTotal As UInteger
+        Dim PCMDateSize As UInteger
+        PCMDateSize = sampleTime * sampleHz * channel
+        Dim sizeByteBuf(PCMDateSize) As Byte
+        fileSizeTotal = PCMDateSize + 44
+        Dim wavH As waveHeaderStructre
+        With wavH
+            .RIFF = "RIFF"
+            .FileSize = fileSizeTotal - 8
+            .WAVE = "WAVE"
+            .FORMAT = "fmt "
+            .FormatSize = 16
+            .FilePadding = 1
+            .FormatChannels = channel
+            .SamplesPerSecond = sampleHz
+            .AverageBytesPerSecond = channel * sampleHz * channelBit / 8
+            .BytesPerSample = channel * channelBit / 8
+            .BitsPerSample = channelBit
+            .DATA = "data"
+            .DataSize = PCMDateSize
+        End With
+        Dim filename As String
+        filename = "d:\" + TextBox5.Text
+        Dim nFile = New FileStream(filename, FileMode.Create, FileAccess.ReadWrite)
+        Dim bw As New BinaryWriter(nFile)
+        For Each abc In wavH.GetType.GetFields
+            bw.Write(abc.GetValue(wavH))
+        Next
+        Dim leftBuf As Byte
+        Dim rightBuf As Byte
+        Select Case channel
+            Case 1
+                For i = 0 To sampleHz * sampleTime - 1
+                    leftBuf = Int(127 * Sin(2 * PI * happenHz * i / sampleHz) + 128) 'And &HFF
+
+                    bw.Write(leftBuf)
+                Next
+            Case 2
+                For i = 0 To sampleHz * sampleTime - 1
+                    rightBuf = Int(127 * Sin(2 * PI * happenHz * i / sampleHz) + 128) 'And &HFF
+                    bw.Write(rightBuf)
+                    leftBuf = Int(127 * Cos(2 * PI * happenHz * i / sampleHz) + 128) 'And &HFF
+                    bw.Write(leftBuf)
+                Next
+            Case Else
+                MsgBox("对不起，目前只支持2个声道！")
+                Return
+        End Select
+        bw.Flush()
+        bw.Close()
+        nFile.Close()
+
+        MsgBox("finsh")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        PictureBox1.Width = 700
+
+        PictureBox1.Height = 80
+        PictureBox2.Width = 700
+        PictureBox2.Height = 80
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Button4.Enabled = True
+        bshowFilename = "D:\" + TextBox5.Text
+        showWaveTrans()
+
+    End Sub
+    Private Sub showWaveTrans()
+        Dim wavdata() As Byte
+        Dim myFile As New IO.FileStream(bshowFilename, FileMode.Open, FileAccess.Read)
+        Dim br As New BinaryReader(myFile)
+        wavdata = br.ReadBytes(myFile.Length)
+        Dim channel As String
+        channel = System.BitConverter.ToUInt16(wavdata, 22)
+        PictureBox1.Refresh()
+        PictureBox2.Refresh()
+
+        Select Case channel
+            Case 1
+                Dim data(wavdata.Length - 45) As Byte
+                Array.ConstrainedCopy(wavdata, 44, data, 0, wavdata.Length - 44)
+                '  MsgBox(Hex(data(0).ToString))
+                showWave（PictureBox1, data)
+            Case 2
+                Dim leftData(Int((wavdata.Length - 45) / 2)) As Byte
+                Dim rightData(Int((wavdata.Length - 45) / 2)) As Byte
+                Dim ddata(wavdata.Length - 45) As Byte
+                Array.ConstrainedCopy(wavdata, 44, ddata, 0, wavdata.Length - 44)
+                Dim k As Integer
+                k = 0
+                For j = 0 To ddata.Length - 1 Step 2
+                    leftData(k) = ddata(j)
+                    rightData(k) = ddata(j + 1)
+                    k = k + 1
+                    'MsgBox("地址：" + Hex(44 + Int(j / 2)).ToString + "---" + Hex(leftData(j).ToString) + "---" + Hex(rightData(j).ToString))
+                Next
+                showWave（PictureBox1, leftData)
+                showWave（PictureBox2, rightData)
+            Case Else
+                MsgBox("通道数超过2，本软件不能解析！")
+        End Select
+
+
+        br.Close()
+        myFile.Close()
+
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        OpenFileDialog1.ShowDialog()
+        bshowFilename = OpenFileDialog1.FileName
+        showWaveTrans()
+        Button4.Enabled = True
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        showWaveTrans()
+    End Sub
+End Class
